5de3a2ff383c4811532dfd8ab59a5528d8717541
Elshadai <elshadaikassutegegn@gmail.com>
2021-06-15 12:39:53 +0300

    Additional adjustments made

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import axios from 'axios'
-import qs from "qs";   
 
 async function getUser(formData){
   try{
-    let res = await axios.post(`https://iproject-api.herokuapp.com/auth/login`, formData)
-    res = await axios.get(`https://iproject-api.herokuapp.com/${res.data.role}/${res.data.user_id}`,
+    let res = await axios.post(`http://127.0.0.1:5000/auth/login`, formData)
+    // let res = await axios.post(`https://iproject-api.herokuapp.com/auth/logout`)
+    console.log(res)
+    res = await axios.get(`http://127.0.0.1:5000/students/2`,
+    // (`https://iproject-api.herokuapp.com/${res.data.role}/${res.data.user_id}`,
     {
       headers: {"Access-Control-Allow-Origin": "*"}
     }
     )
+    // let res = await axios.get(`https://iproject-api.herokuapp.com/students/`,
+    // {
+    //   headers: {"Access-Control-Allow-Origin": "*"}
+    // }
+    // )
+
   console.log(res)
   return res   }
   catch(err){
@@ -39,21 +47,8 @@ export default class Login extends Component {
     const formData = new FormData()
     formData.append('email', this.email);
     formData.append('password', this.password);
-    for (var pair of formData.entries()) {
-      console.log(pair[0]+ ', ' + pair[1]); 
-  }
 
-  getUser(formData).then(res => {console.log(res)})
-  // console.log(result)
-  // fetch("https://iproject-api.herokuapp.com/students/", {
-  //   method: 'get'
-  // }).then(res => res.json())
-  //   .then(
-  //   (result) => {
-  //       console.log(result);
-  //   }).catch(err => {
-  //       console.log(err);
-  //   })
+  getUser(formData).then(res => {console.log(res)}).catch(err => console.log(err))
 
   }
   render() {
